eefe3577fc22367b18b0260ee33f9baafeb35e4d
Eyder Suarez <ey_der19@hotmail.com>
2024-11-18 15:51:10 -0500

    Fixes login

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  imports: [ CommonModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule]
+  imports: [ ]
 })
 export class DashboardComponent implements OnInit {
   isCollapsed = false;
